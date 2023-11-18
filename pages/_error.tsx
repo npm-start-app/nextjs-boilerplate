d6cd7ac4b8a849193d8ded20dff9949d83c70831
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2023-11-19 01:08:40 +0200

    Update _error.tsx

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -20,7 +20,7 @@ const ErrorPage = ({ statusCode } : ErrorPageOb1) => {
                 <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                     <Link
-                        href="/"
+                        href="https://nextjs-boilerplate-three-blond-44.vercel.app"
                         className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         Main Page
